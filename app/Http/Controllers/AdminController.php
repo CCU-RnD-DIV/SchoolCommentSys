57cdb90113cf5bea55a71b3236f9ee7f6a590c9f
onemid <onemid85@gmail.com>
2016-05-19 12:04:58 +0800

    forceRedirect

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -140,7 +140,7 @@ class AdminController extends Controller
 
             $comment_detail = Comment::where('id', $id)->where('sid', Auth::user()->account)->get();
 
-            if (!isset($comment_detail[0]->id)) return redirect('/general');
+            if ($comment_detail[0]->id != NULL) return redirect('/general');
 
             $aca_user_detail = DB::connection('pgsql')
                 ->table('a11vstd_rec_tea')
